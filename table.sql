3923117d29ef76c55383d0c5a895a04d6928c6f2
Ian Ruvuto <66079287+Tallerian@users.noreply.github.com>
2022-10-13 12:26:16 +0200

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,3 +1,4 @@
+    DROP TABLE IF EXISTS train; 
     CREATE TABLE train (
         train_id INT, 
         train_name VARCHAR(20) UNIQUE, 
@@ -5,7 +6,8 @@
         destination VARCHAR(20),    
         PRIMARY KEY(train_id)
     );
-
+    
+    DROP TABLE IF EXISTS regional; 
     CREATE TABLE regional(
         Reg_id INT,
         train_name VARCHAR(20),
@@ -15,7 +17,8 @@
         FOREIGN KEY(train_name) REFERENCES train(train_name)ON DELETE CASCADE,
         FOREIGN KEY(route_id) REFERENCES route(route_id)
     );
-
+    
+    DROP TABLE IF EXISTS long_Dist;
     CREATE TABLE long_Dist(
         long_id INT,
         train_name VARCHAR(20),
@@ -25,7 +28,8 @@
         FOREIGN KEY(train_name) REFERENCES train(train_name)ON DELETE CASCADE,
         FOREIGN KEY(route_id) REFERENCES route(route_id)
     );
-
+    
+    DROP TABLE IF EXISTS schedule;
     CREATE TABLE schedule (
         train_id INT,
         sched_id INT,
@@ -34,7 +38,8 @@
         PRIMARY KEY(Sched_id),
         FOREIGN KEY (train_id) REFERENCES train
     );
-
+    
+    DROP TABLE IF EXISTS route;
     CREATE TABLE route(
         train_id INT,
         route_id INT UNIQUE,
@@ -43,20 +48,23 @@
         FOREIGN KEY (train_id) REFERENCES train(train_id)
     );
 
+    DROP TABLE IF EXISTS station;
     CREATE TABLE station (
         station_id INT, 
         s_name varchar(30) UNIQUE,
         PRIMARY KEY(station_id)
     );
-
+    
+    DROP TABLE IF EXISTS route_path;
     CREATE TABLE route_path (
         route_id INT,
         s_name VARCHAR(55),
         sequences INT,
         FOREIGN KEY (route_id) REFERENCES route(route_id),
         FOREIGN KEY (s_name) REFERENCES station(s_name)
-    );
-
+    );  
+    
+    DROP TABLE IF EXISTS passenger;
     CREATE TABLE passenger (
         p_id INT, 
         p_name VARCHAR(20), 
@@ -64,39 +72,43 @@
         destination INT, 
         PRIMARY KEY(p_id)
     );
-
+    
+    DROP TABLE IF EXISTS student;
     CREATE TABLE student (
         p_id INT, 
         student_id VARCHAR(20),
         FOREIGN KEY(p_id) REFERENCES passenger(p_id)ON DELETE CASCADE
     );
-
+    
+    DROP TABLE IF EXISTS Adult;
     CREATE TABLE Adult (
         p_id INT,
         adult_id VARCHAR(20),
         FOREIGN KEY(p_id) REFERENCES passenger(p_id)ON DELETE CASCADE
     );
 
-    
+    DROP TABLE IF EXISTS school_Student;
     CREATE TABLE school_Student(
         p_id INT, 
         school_disc INT,
         FOREIGN KEY(p_id) REFERENCES passenger(p_id)ON DELETE CASCADE
     );
-
+    
+    DROP TABLE IF EXISTS Uni_Student;
     CREATE TABLE Uni_Student(
         p_id INT, 
         uni_disc INT,
         FOREIGN KEY(p_id) REFERENCES passenger(p_id)ON DELETE CASCADE
     );
 
-
+    DROP TABLE IF EXISTS Worker;
     CREATE TABLE Worker(
         p_id INT, 
         work_disc INT,
         FOREIGN KEY(p_id) REFERENCES passenger(p_id)ON DELETE CASCADE
     );
-
+    
+    DROP TABLE IF EXISTS Retire;
     CREATE TABLE Retire(
         p_id INT, 
         retire_disc INT,
